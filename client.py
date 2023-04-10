e8270f3a75e03106ae88a2bffa42d722c361e5fd
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-10 09:56:10 +0300

    private messages and leave chat

--- client.py
+++ client.py
@@ -10,16 +10,21 @@ client.connect(("127.0.0.1", 8000))
 # Function to listen to server and choose a nickname
 def receive():
     while True:
-        msg = client.recv(1024).decode("ascii")
-        if (msg == "nickname"):
-            client.send((nickname).encode("ascii"))
-        else:
-            print(msg)
+        try:
+            msg = client.recv(1024).decode("ascii")
+            if (msg == "nickname"):
+                client.send((nickname).encode("ascii"))
+            else:
+                print(msg)
+        except:
+            print("Error. Closing connection.\n")
+            client.close()
+            break
 
 # Function to send massages to server
 def write_msg():
     while True:
-        msg = "{}: {}\n".format(nickname, input(""))
+        msg = "{}: {}".format(nickname, input(""))
         client.send(msg.encode("ascii"))
 
 # Start threads for listening and writing
--- server.py
+++ server.py
@@ -22,12 +22,33 @@ def public_message(msg):
     for client in clients:
         client.send(msg)
 
+# Send message to one client
+def private_message(msg):
+    for client in clients:
+        idx = clients.index(client)
+        if ("//pm " + nicknames[idx]) in msg:
+            msg = msg.replace("//pm " + nicknames[idx], "[pm]")
+            client.send(msg.encode("ascii"))
+
 # Function to handle messages from clients
 # (and remove clients if needed)
 def handle(client):
     while True:
-        msg = client.recv(1024)
-        public_message(msg)
+        try:
+            msg = client.recv(1024).decode("ascii")
+            if "//quit" in msg:
+                print("quit")
+                remove(client)
+                break
+            elif "//pm" in msg:
+                print("pm")
+                private_message(msg)
+            else:
+                public_message(msg.encode("ascii"))
+        except:
+            remove(client)
+            print(clients)
+            break
 
 # Receiving/listening function
 def receive():
@@ -42,12 +63,22 @@ def receive():
         nicknames.append(nickname)
 
         # Send info about the new chatter to other users
-        msg = nickname + " joined the chat!"
-        public_message(msg.encode("ascii"))
         client.send("Connected to server.\n".encode("ascii"))
+        msg = nickname + " joined the chat!\n"
+        public_message(msg.encode("ascii"))
 
         # Create a thread for the client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+# Function to remove a client
+def remove(client):
+    if client in clients:
+        idx = clients.index(client)
+        clients.remove(client)
+        client.close()
+        print(nicknames[idx], "disconnected")
+        public_message(("{} left the chat.\n".format(nicknames[idx])).encode("ascii"))
+        del nicknames[idx]
 
 receive()
